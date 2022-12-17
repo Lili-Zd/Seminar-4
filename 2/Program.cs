f4015238df54f69ebec0d9519ad0802a8d8afc81
Lili-Zd <zdanova.liliya@gmail.com>
2022-12-17 22:05:54 +0300

    Dobavlena papka 2

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Реализовать через функции.
+
+int Promt(string massage)
+{
+    Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int total(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum+= number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int number = Promt("Input number");
+
+System.Console.WriteLine($"Sum of number is {total(number)}");
